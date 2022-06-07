2123c375b3a59c34292d854f88d4f0a772673c51
root <abiol4001@gmail.com>
2022-06-06 21:21:25 -0700

    More edit on divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return my_list
-    new_list = my_list[:]
-    for i in new_list:
-        if i % 2 == 0:
-            new_list[i] = True
-        else:
-            new_list[i] = False
-    return new_list
+    blist = [a % 2 == 0 for a in my_list]
+    return blist
