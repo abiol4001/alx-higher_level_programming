a7eb5c046ae3d76722d4b72efa0af8a20393c14c
root <abiol4001@gmail.com>
2022-06-06 21:08:55 -0700

    Print matrix int

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    if idx > len(my_list):
-        return my_list
-    temp_list = my_list[:]
-    temp_list[idx] = element
-    return temp_list
+    dup = my_list[:]
+    if idx >= 0 and idx < len(dup):
+        dup[idx] = element
+        return dup
