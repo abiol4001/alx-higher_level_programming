eb5ac22fd63c53dcb3d4f8ab7bc89fd0cf972370
root <abiol4001@gmail.com>
2022-06-07 17:50:12 -0700

    Print hexa

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(ord('a'), ord('z') + 1):
-    if char(a) is not 'q' and char(a) is not 'e':
+    if chr(a) is not 'q' and chr(a) is not 'e':
         print("{:s}".format(char(a)), end="")
