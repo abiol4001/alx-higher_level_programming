c4e1ea825fd0f9a7bf092d49c7f2dccb0c59b6c3
root <abiol4001@gmail.com>
2022-06-10 17:58:00 -0700

    Amendment

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/urs/bin/python3
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda r: list(map(lambda x: x**2, r)), matrix)))
+    return list(map(lambda r: list(map(lambda x: x**2, r)), matrix))
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k in (a_dictionar.keys()):
+    for k in list(a_dictionar.keys()):
         if a_dictionary[k] is value:
             del a_dictionary[k]
     return a_dictionary
